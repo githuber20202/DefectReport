2f99097bdb089dd45957752d69db7dbf94fdc766
githuber20202 <59474973+githuber20202@users.noreply.github.com>
2025-03-07 21:46:57 +0200

    try use other ports on render not only 3000p

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const XLSX = require('xlsx');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // ✅ הגדרות CORS
 app.use(cors());
